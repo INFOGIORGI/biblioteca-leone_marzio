afbfec973fc72fb53fa0d67329d729e8aee8f7f9
edoardo <105643778+Scapizza@users.noreply.github.com>
2025-02-17 11:48:52 +0000

    Changes to be committed: modified:   app.py modified:   db.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask,render_template
+from flask_mysqldb import MySQL
 from db import DB #importo la classe DB
 
 app = Flask(__name__)
 db = DB() #creo un oggetto della classe DB con il costruttore che crea le tabelle se già non esistono
 
 
+    app = Flask(__name__)
+    app.config['MYSQL_HOST']="138.41.20.102"
+    app.config['MYSQL_PORT']=53306
+    app.config['MYSQL_USER']="ospite"
+    app.config['MYSQL_PASSWORD']="ospite"
+    app.config['MYSQL_DB']="leone_marzio"
+    mysql= MySQL(app)
 
 @app.route("/")
 def hello():
